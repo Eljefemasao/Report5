b9934c6efbf4ba339461a986bd5d92386bf868e4
Masaaki Matsunaga <castle2164m@gmail.com>
2017-01-01 18:31:59 +0900

    programing2_report5

---
+++ src/jp/ac/uryukyu/ie/e145165/report5.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e145165;
+
+/**
+ * Created by masaaki on 2017/01/01.
+ */
+public class report5 {
+    public static void main(String args[]){
+        String str;
+        str = null;
+        System.out.println(str.length());
+
+    }
+}
